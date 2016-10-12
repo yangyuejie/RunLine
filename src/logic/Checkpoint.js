40ddacb8ca747584bb8cdfc6de9ae4b6e3db5160
yuejie yang <yuejie.yang@joy4you.com>
2016-10-12 23:56:40 +0800

    修改框架功能

--- src/logic/Checkpoint.js
+++ src/logic/Checkpoint.js
@@ -4,20 +4,58 @@
 
 var Checkpoint = {
 
+    levelData:[],
+
     //加载关卡数值信息
     loadCardData: function(loadCallback){
-        //var res = [];
-        //for(var i=1; i<cardNum+1; i++){
-        //    res.push("res/data/checkpoint_"+i+".txt");
-        //}
-        //var self = this;
-        //cc.loader.load(res, function(err,array){
-        //    loadCallback(array);
-        //});
+
+        //管卡单元格id信息数组
+        this.levelData = [];
+
+        var res = [];
+        for(var i=1; i<cardNum+1; i++){
+            res.push("res/data/level_"+i+".txt");
+        }
+        var self = this;
+        cc.loader.load(res, function(err,array){
+            var inforArr = [];
+            array = array[0].split("\n");
+            for(var i=0; i<array.length; i++){
+                var itemArr = array[i].split(",");
+                inforArr.push(itemArr);
+            }
+           // cc.log(inforArr);
+            self.levelData.push(inforArr);
+           // loadCallback(array);
+        });
 
         cc.loader.load("res/data/unitInfor.txt", function(err,array){
+
+            var inforArr = [];
+            array = array[0].split("\n");
             for(var i=0; i<array.length; i++){
-                cc.log(array[i]);
+                var itemArr = array[i].split(",");
+                inforArr.push(itemArr);
+            }
+
+            for(var j=0; j<inforArr.length; j++){
+                var data = inforArr[j];
+                var item = new ItemInfor();
+                item.setItemID(data[0]);
+                item.setRouteID(data[1]);
+                item.setMoveDir(data[2]);
+                item.setExtendStep(data[3]);
+                item.setRotateDir(data[4]);
+                item.setMoveSpeed(data[5]);
+                item.setRelatePos(data[6]);
+                item.setIfStop(data[7]);
+                item.setIfTarget(data[8]);
+                item.setIfChangeDir(data[9]);
+                item.setIfDisplay(data[10]);
+                item.setItemColor(data[11]);
+
+                //单元格信息数组
+                ItemInforList.shared().addObject(item);
             }
         });
     }
